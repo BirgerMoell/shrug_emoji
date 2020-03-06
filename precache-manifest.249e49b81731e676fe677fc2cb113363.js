a87a81df00d62dbaaf03c52ba4ce3179a2771ed0
Birger Moëll <birger.moell@gmail.com>
2020-03-06 23:22:54 +0100

    Updates

---
+++ precache-manifest.249e49b81731e676fe677fc2cb113363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6752a672b5d9ee99d816ea6d8fffd970",
+    "url": "/shrug_emoji/index.html"
+  },
+  {
+    "revision": "fc09121ead192c4fba71",
+    "url": "/shrug_emoji/static/css/main.ae942628.chunk.css"
+  },
+  {
+    "revision": "ae06f7d364adf73ca911",
+    "url": "/shrug_emoji/static/js/2.145f1b74.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/shrug_emoji/static/js/2.145f1b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc09121ead192c4fba71",
+    "url": "/shrug_emoji/static/js/main.c8e23c35.chunk.js"
+  },
+  {
+    "revision": "6743baba76190bf23743",
+    "url": "/shrug_emoji/static/js/runtime-main.70d9476f.js"
+  },
+  {
+    "revision": "bf221a28cf6d15c47912abe0437c1398",
+    "url": "/shrug_emoji/static/media/shrug.bf221a28.jpg"
+  }
+]);
