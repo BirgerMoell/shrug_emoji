6019708ffa696a721d1269dc827c4563e532baec
Birger Moëll <birger.moell@gmail.com>
2020-03-06 23:16:25 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import shrug_emoji from './shrug.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a target="_blank" href="https://www.avanza.se/aktier/topplistor.html/PRrDuDZT">
+        <img src={shrug_emoji} className="App-logo" alt="logo" />
         </a>
+        <p>
+         Buy stuff!
+        </p>
+
+        <p>P.S. Don't buy stuff</p>
+
+
       </header>
     </div>
   );
